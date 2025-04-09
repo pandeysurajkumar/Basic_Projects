dbf3883cd371878829055c039245c92681c2670c
Suraj Kumar Pandey <pandeysurajkumar265@gmail.com>
2025-04-09 17:55:46 +0530

    BMI calculater taking ref. of Chai or Code  in html ,css,and js

--- 2-BMICalculator/javascript.js
+++ 2-BMICalculator/javascript.js
@@ -1,38 +1,3 @@
-# Project related to DOM
-
-
-## project link 
-[Click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution Code
-
-## Project 1
-
-```javascript
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
-
-buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-        if(e.target.id === 'grey'){
-            body.style.backgroundColor = e.target.id;
-        }
-        if(e.target.id === 'white'){
-            body.style.backgroundColor = e.target.id;
-        }
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id;
-        }
-        if(e.target.id === 'yellow'){
-            body.style.backgroundColor = e.target.id;
-        }
-    });
-});
-
-```
-
-## Project 2
-```javascript
 const form = document.querySelector('form')
 // this usecase will give you empty
 // const height = parseInt(document.querySelector('#height').value)
@@ -70,5 +35,3 @@ form.addEventListener('submit', function (e) {
     }
     
 });
-
-```
